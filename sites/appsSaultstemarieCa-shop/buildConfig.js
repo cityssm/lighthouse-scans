fdd895cbb44e18a5f7143b0118176fc2bce26f0f
dangowans <d.gowans@cityssm.on.ca>
2021-11-03 14:21:00 -0400

    add ticket product pages

--- sites/appsSaultstemarieCa-shop/buildConfig.js
+++ sites/appsSaultstemarieCa-shop/buildConfig.js
@@ -3,6 +3,9 @@ import { writeConfig } from "../../utils.js";
     await writeConfig([
         "https://apps.saultstemarie.ca/cityapps/shop/products",
         "https://apps.saultstemarie.ca/cityapps/shop/products/tax_receipt",
+        "https://apps.saultstemarie.ca/cityapps/shop/products/ticket_offenceNotice",
+        "https://apps.saultstemarie.ca/cityapps/shop/products/ticket_noticeOfFine",
+        "https://apps.saultstemarie.ca/cityapps/shop/products/ticket_parking",
         "https://apps.saultstemarie.ca/cityapps/shop/checkout"
     ], [], "appsSaultstemarieCa-shop");
 })();
