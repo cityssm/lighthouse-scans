67192b67bc70fd288774764d19a99c223191a1df
dangowans <d.gowans@cityssm.on.ca>
2022-02-01 15:36:23 -0500

    add more documentation

--- sites/cityssmGithubIO/buildConfig.ts
+++ sites/cityssmGithubIO/buildConfig.ts
@@ -3,12 +3,16 @@ import { writeConfig } from "../../utils.js";
 (async () => {
 
   await writeConfig([
-    "https://cityssm.github.io/",
-    "https://cityssm.github.io/corporate-records-manager/",
-    "https://cityssm.github.io/lottery-licence-manager/"
+    "https://cityssm.github.io",
+    "https://cityssm.github.io/contract-expiration-tracker",
+    "https://cityssm.github.io/corporate-records-manager",
+    "https://cityssm.github.io/general-licence-manager",
+    "https://cityssm.github.io/lottery-licence-manager"
   ], [
-      "https://cityssm.github.io/corporate-records-manager/",
-      "https://cityssm.github.io/lottery-licence-manager/"
+      "https://cityssm.github.io/contract-expiration-tracker",
+      "https://cityssm.github.io/corporate-records-manager",
+      "https://cityssm.github.io/general-licence-manager",
+      "https://cityssm.github.io/lottery-licence-manager"
     ],
     "cityssmGithubIO");
 })();
