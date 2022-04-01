16e7a8828c95cb98bb59fedbfe392d7cc8286a63
dangowans <d.gowans@cityssm.on.ca>
2022-04-01 14:04:57 -0400

    update build config

--- sites/cityssmGithubIO/buildConfig.js
+++ sites/cityssmGithubIO/buildConfig.js
@@ -2,14 +2,12 @@ import { writeConfig } from "../../utils.js";
 (async () => {
     await writeConfig([
         "https://cityssm.github.io",
-        "https://cityssm.github.io/contract-expiration-tracker",
-        "https://cityssm.github.io/corporate-records-manager",
-        "https://cityssm.github.io/general-licence-manager",
-        "https://cityssm.github.io/lottery-licence-manager"
+        "https://cityssm.github.io/municipal-github-rankings/"
     ], [
         "https://cityssm.github.io/contract-expiration-tracker",
         "https://cityssm.github.io/corporate-records-manager",
         "https://cityssm.github.io/general-licence-manager",
-        "https://cityssm.github.io/lottery-licence-manager"
+        "https://cityssm.github.io/lottery-licence-manager",
+        "https://cityssm.github.io/parking-ticket-system/"
     ], "cityssmGithubIO");
 })();
--- sites/cityssmGithubIO/buildConfig.ts
+++ sites/cityssmGithubIO/buildConfig.ts
@@ -4,15 +4,13 @@ import { writeConfig } from "../../utils.js";
 
   await writeConfig([
     "https://cityssm.github.io",
-    "https://cityssm.github.io/contract-expiration-tracker",
-    "https://cityssm.github.io/corporate-records-manager",
-    "https://cityssm.github.io/general-licence-manager",
-    "https://cityssm.github.io/lottery-licence-manager"
+    "https://cityssm.github.io/municipal-github-rankings/"
   ], [
       "https://cityssm.github.io/contract-expiration-tracker",
       "https://cityssm.github.io/corporate-records-manager",
       "https://cityssm.github.io/general-licence-manager",
-      "https://cityssm.github.io/lottery-licence-manager"
+      "https://cityssm.github.io/lottery-licence-manager",
+      "https://cityssm.github.io/parking-ticket-system/"
     ],
     "cityssmGithubIO");
 })();
