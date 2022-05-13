16c78e28affd218c476d905fe271a0df38b39d11
dangowans <d.gowans@cityssm.on.ca>
2022-05-13 08:15:12 -0400

    exclude a page with a youtube video

--- sites/saultstemarie/buildConfig.ts
+++ sites/saultstemarie/buildConfig.ts
@@ -13,7 +13,9 @@ import { writeConfig } from "../../utils.js";
       "https://saultstemarie.ca"
     ],
     [
-      "https://saultstemarie.ca/Newsroom/November-2019/Celebrate-the-season-with-the-annual-Community-Tre.aspx"
+      // Include YouTube videos that fail performance test
+      "https://saultstemarie.ca/Newsroom/November-2019/Celebrate-the-season-with-the-annual-Community-Tre.aspx",
+      "https://saultstemarie.ca/Newsroom/April-2020/Messages-of-encouragement-from-notable-community-m.aspx"
     ],
     "saultstemarie");
 })();
