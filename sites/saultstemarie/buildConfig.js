ad4e2317e6cd130dddbde7e1b8aa062ccccf7e73
dangowans <d.gowans@cityssm.on.ca>
2022-06-07 08:16:04 -0400

    exclude page with youtube video

--- sites/saultstemarie/buildConfig.js
+++ sites/saultstemarie/buildConfig.js
@@ -10,6 +10,7 @@ import { writeConfig } from "../../utils.js";
     ], [
         "https://saultstemarie.ca"
     ], [
+        "https://saultstemarie.ca/Government/City-Departments/Community-Development-Enterprise-Services/FutureSSM/Film-Office.aspx",
         "https://saultstemarie.ca/Newsroom/April-2020/Messages-of-encouragement-from-notable-community-m.aspx",
         "https://saultstemarie.ca/Newsroom/November-2019/Celebrate-the-season-with-the-annual-Community-Tre.aspx",
         "https://saultstemarie.ca/Newsroom/October-2019/Mayor-for-a-Day-contest-winner-announced.aspx",
--- sites/saultstemarie/buildConfig.ts
+++ sites/saultstemarie/buildConfig.ts
@@ -12,12 +12,13 @@ import { writeConfig } from "../../utils.js";
   ], [
       "https://saultstemarie.ca"
     ], [
-      // Exclude YouTube videos that fail performance test
+      // Exclude pages with YouTube videos that fail performance tests
+      "https://saultstemarie.ca/Government/City-Departments/Community-Development-Enterprise-Services/FutureSSM/Film-Office.aspx",
       "https://saultstemarie.ca/Newsroom/April-2020/Messages-of-encouragement-from-notable-community-m.aspx",
       "https://saultstemarie.ca/Newsroom/November-2019/Celebrate-the-season-with-the-annual-Community-Tre.aspx",
       "https://saultstemarie.ca/Newsroom/October-2019/Mayor-for-a-Day-contest-winner-announced.aspx",
 
-      // Exclude Google Maps 360 Views that fail performance test
+      // Exclude pages with Google Maps 360 Views that fail performance tests
       "https://saultstemarie.ca/Government/City-Council/Council-Chambers.aspx"
     ],
     "saultstemarie");
