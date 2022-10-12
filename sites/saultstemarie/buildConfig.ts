2deea9ff8063e6aad769b99fb2438bc5f5915278
Dan Gowans <d.gowans@cityssm.on.ca>
2022-10-12 08:25:36 -0400

    skip scanning page with youtube video

--- sites/saultstemarie/buildConfig.ts
+++ sites/saultstemarie/buildConfig.ts
@@ -14,6 +14,7 @@ import { writeConfig } from "../../utils.js";
     ], [
       // Exclude pages with YouTube videos that fail performance tests
       "https://saultstemarie.ca/Government/City-Departments/Community-Development-Enterprise-Services/FutureSSM/Film-Office.aspx",
+      "https://saultstemarie.ca/Government/City-Departments/Corporate-Services/City-Clerk/Municipal-Election/Election.aspx",
       "https://saultstemarie.ca/Newsroom/April-2020/Messages-of-encouragement-from-notable-community-m.aspx",
       "https://saultstemarie.ca/Newsroom/November-2019/Celebrate-the-season-with-the-annual-Community-Tre.aspx",
       "https://saultstemarie.ca/Newsroom/October-2019/Mayor-for-a-Day-contest-winner-announced.aspx",
