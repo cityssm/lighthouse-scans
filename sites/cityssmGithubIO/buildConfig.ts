273d942e4253ec029b5dd668891fcf5aa778192f
Dan Gowans <dangowans@users.noreply.github.com>
2024-02-06 14:03:19 -0500

    add tip of the month

--- sites/cityssmGithubIO/buildConfig.ts
+++ sites/cityssmGithubIO/buildConfig.ts
@@ -1,23 +1,23 @@
-import { writeConfig } from "../../utils.js";
-
-(async () => {
+import { writeConfig } from '../../utils.js'
+;(async () => {
   await writeConfig(
     [
-      "https://cityssm.github.io",
-      "https://cityssm.github.io/municipal-github-rankings/",
-      "https://cityssm.github.io/property-tax-rate-data/",
-      "https://cityssm.github.io/ward-finder/"
+      'https://cityssm.github.io',
+      'https://cityssm.github.io/municipal-github-rankings/',
+      'https://cityssm.github.io/property-tax-rate-data/',
+      'https://cityssm.github.io/ward-finder/'
     ],
     [
-      "https://cityssm.github.io/contract-expiration-tracker",
-      "https://cityssm.github.io/corporate-records-manager",
-      "https://cityssm.github.io/general-licence-manager",
-      "https://cityssm.github.io/lot-occupancy-system/",
-      "https://cityssm.github.io/lottery-licence-manager",
-      "https://cityssm.github.io/attendance-tracking/",
-      "https://cityssm.github.io/parking-ticket-system/"
+      'https://cityssm.github.io/contract-expiration-tracker',
+      'https://cityssm.github.io/corporate-records-manager',
+      'https://cityssm.github.io/general-licence-manager',
+      'https://cityssm.github.io/lot-occupancy-system/',
+      'https://cityssm.github.io/lottery-licence-manager',
+      'https://cityssm.github.io/attendance-tracking/',
+      'https://cityssm.github.io/parking-ticket-system/',
+      'https://cityssm.github.io/tip-of-the-month/'
     ],
     [],
-    "cityssmGithubIO"
-  );
-})();
+    'cityssmGithubIO'
+  )
+})()
